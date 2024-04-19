c5cb76da1d6d8a2ee69f1166fe1ea5587bbac46b
Artemiy111 <mrart111@mail.ru>
2024-04-19 09:37:37 +0300

    refactor: create SelectVariant component

---
+++ components/SelectVariant.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts" generic="Variant extends BaseVariant & Record<string, unknown>">
+import type { HTMLAttributes } from 'vue'
+import type { cn } from '~/lib/utils'
+import type { BaseVariant } from '~/math/variants'
+
+export interface SelectVariantHeader {
+  title: string
+  key: string
+}
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+  headers: SelectVariantHeader[]
+  variants: Variant[]
+}>()
+
+const emit = defineEmits<{
+  'select:variant': [variant: Variant]
+}>()
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button variant="secondary" :class="props.class">
+        Варианты
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="max-h-[70dvh] w-full overflow-y-auto">
+      <Table class="w-max">
+        <TableHeader>
+          <TableRow>
+            <TableHead v-for="header in props.headers" :key="header.key">
+              {{ header.title }}
+            </TableHead>
+          </TableRow>
+          <TableRow
+            v-for="variant in props.variants" :key="variant.order" class="cursor-pointer" tabindex="0"
+            @keypress.enter.space="emit('select:variant', variant)"
+            @click="emit('select:variant', variant)"
+          >
+            <TableCell v-for="{ key } in props.headers" :key="key">
+              {{ variant[key] }}
+            </TableCell>
+          </TableRow>
+        </TableHeader>
+      </Table>
+    </PopoverContent>
+  </Popover>
+</template>
